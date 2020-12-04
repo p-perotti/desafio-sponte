ad4cb8bb78da27e2def0638d35a8bb91fa382d59
Patrick Alexis Perotti <patrickperotti0@gmail.com>
2020-12-04 16:00:30 -0300

    unused var

--- src/components/ImageInput/index.js
+++ src/components/ImageInput/index.js
@@ -1,5 +1,5 @@
 /* eslint react/prop-types: 0 */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FiPlus } from 'react-icons/fi';
 
